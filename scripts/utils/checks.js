7e037e799218aac5556c85bb423c5ced5a8d379c
Jamie Smith <i@jamiesmiths.com>
2019-11-06 20:44:50 -0500

    fix: if select w/o users, run refresh then continue

--- scripts/utils/checks.js
+++ scripts/utils/checks.js
@@ -1,6 +1,6 @@
 module.exports = {
     usernames: users => {
-        const check = users || users.length > 0
+        const check = users && users.length > 0
         if(!check){
             console.log(`\n there were no usernames cached from sfdx 🐭 \n`);
         }

05e91b9ff5a3da49fbc70d8350b6544a3484d7bb
Jamie Smith <i@jamiesmiths.com>
2020-02-22 01:13:34 -0500

    #6 cache file to os home dir

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ process.env.HOME_DIR = require('os').homedir()
 const Store = require('data-store')
 const Router = require('./scripts/Router.js')
 
-const app = new Store({ path: 'store.json' })
+const app = new Store({ path: process.env.HOME_DIR+'/.metamouse.json' })
 
 Router.init(app)
